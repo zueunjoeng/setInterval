4505b0065d85e4cffe304942acc9e4edf6c4a2fd
zueunjoeng <judms0314@gmail.com>
2024-06-26 11:53:37 +0900

    autobanner선언 후 버튼생성->클릭이벤트로 슬라이드 멈추기

--- setInterval.js
+++ setInterval.js
@@ -1,36 +1,37 @@
-// let count =0;
-// const navidata=["네이버", "구글", "다음", "깃", "아마존"]
 
-// setInterval(function(){
-//  console.log(count);
-// count++;
-// }, 500)
+// 자바스크립트 경로는 css 다른다!!!! 연결된 html 기준으로 처리해야한다.
 
-// setInterval(function(){
-//     console.log(navidata[count]);
-//     count++;
-//     count %= navidata.length;
-// }, 500)
-
-
-//자바스크립트 경로는 css와 다르다!!!! 연결된 html 기준으로 처리해야한다!!!!
-// let count=3;
+// let count  = 3;
 // setInterval(function(){
 //     count++;
 //     count %= 5;
-//     document.querySelector("#bannerimg").setAttribute("src", `/banner/${count}.png`)
+//     document.querySelector("#bannerimg").setAttribute("src",`/banner/${count}.png`)
 // }, 1000);
 
-const bannerdata=[
+//$("#bannerimg").attr("src","/banner/1.png")
+
+const bannerdata = [
     "/banner2/1.png",
     "/banner/0.png",
-    "/banner2/event.png"
+    "/banner2/membership.png"
 ];
 
-//같은변수
-let num=0;
-setInterval(function(){
+//같은 변수 
+let num = 0;
+document.querySelector("#bannerimg").setAttribute("src",bannerdata[num])
+
+document.querySelector(".bannerwrap").style.background =`
+                           url(${bannerdata[num]}) center no-repeat`;
+
+
+const autobanner = setInterval(function(){
     num++;
-    num %= bannerdata.length
-    document.querySelector("#bannerimg").setAttribute("src", bannerdata[num]
-)},1000)
+    num %= bannerdata.length;
+    document.querySelector(".bannerwrap").style.background =`url(${bannerdata[num]}) center no-repeat`;
+    document.querySelector("#bannerimg").setAttribute("src",bannerdata[num]) 
+}, 3000)
+
+document.querySelector(".bannerwrap button").addEventListener('click', function(){
+    clearInterval(autobanner);
+    this.innerHTML="PLAY"
+})
