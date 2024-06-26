d82c2b8a34ba9d34af2d4a813341462035913053
zueunjoeng <judms0314@gmail.com>
2024-06-26 11:05:03 +0900

    setInterval- array로 슬라이드돌리기

--- setInterval.js
+++ setInterval.js
@@ -14,10 +14,23 @@
 
 
 //자바스크립트 경로는 css와 다르다!!!! 연결된 html 기준으로 처리해야한다!!!!
-let count=3;
-setInterval(function(){
-    count++;
-    count %= 5;
-    document.querySelector("#bannerimg").setAttribute("src", `/banner/${count}.png`)
-}, 1000);
+// let count=3;
+// setInterval(function(){
+//     count++;
+//     count %= 5;
+//     document.querySelector("#bannerimg").setAttribute("src", `/banner/${count}.png`)
+// }, 1000);
 
+const bannerdata=[
+    "/banner2/1.png",
+    "/banner/0.png",
+    "/banner2/event.png"
+];
+
+//같은변수
+let num=0;
+setInterval(function(){
+    num++;
+    num %= bannerdata.length
+    document.querySelector("#bannerimg").setAttribute("src", bannerdata[num]
+)},1000)
